c3ae980a98dc743f5696a6872b93e262dfca10f6
Tudor <tudoralbu2@yahoo.com>
2022-03-31 07:19:09 +0300

    Laboratorul 4 Tema 2

---
+++ TemaAcasa2/TemaAcasa2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaAcasa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                Console.Write("Linia de comanda nu contine argumente");
+            else
+            {
+                int first_letter_int;
+                int row;
+                int[] nr_elementsOnRow = new int[26];
+                Console.WriteLine("Numarul de argumente este: {0}", args.Length);
+
+                string[][] vector = new string[26][];
+                for (int contor = 0; contor < 26; contor++)
+                    vector[contor] = new string[args.Length];
+
+                for (int contor = 0; contor < 26; contor++)
+                    nr_elementsOnRow[contor] = 0;
+
+                foreach (string param in args)
+                {
+                    first_letter_int = Convert.ToInt32(param[0]);
+                    if ((65 <= first_letter_int) && (first_letter_int <= 90))
+                    {
+                        row = first_letter_int - 65;
+                        vector[row][nr_elementsOnRow[row]] = param;
+                        nr_elementsOnRow[row] = nr_elementsOnRow[row] + 1;
+                    }
+                    else
+                    {
+                        row = first_letter_int - 97;
+                        vector[row][nr_elementsOnRow[row]] = param;
+                        nr_elementsOnRow[row] = nr_elementsOnRow[row] + 1;
+                    }
+                }
+
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    if (nr_elementsOnRow[i] != 0)
+                    {
+                        for (int j = 0; j < nr_elementsOnRow[i]; j++)
+                            Console.Write("{0} ", vector[i][j]);
+                        Console.WriteLine();
+                    }
+                         
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
